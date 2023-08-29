433770f1b3b52aac57afb930ef3a4fdaf4d38bce
Gianni <gianniicherry@gmail.com>
2023-08-29 11:54:05 -0700

    added weight

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
     attribute :name, :string
     attribute :brand, :string
+    attribute :weight, :float
 
     has_many :ewastes
     has_many :requests, through: :ewastes
