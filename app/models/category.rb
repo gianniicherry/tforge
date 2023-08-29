cd1362a9f7213c421308ba5090496b17b22a2ed9
Gianni <gianniicherry@gmail.com>
2023-08-29 11:48:36 -0700

    updated attr's

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
     attribute :name, :string
-    
+    attribute :brand, :string
+    attribute :weight, :float
+    attribute :price, :integer
+
     has_many :ewastes
     has_many :requests, through: :ewastes
 end
