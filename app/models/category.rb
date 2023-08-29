9fd07192e8d18f77e19505a02b1f0d66d3905470
Gianni <gianniicherry@gmail.com>
2023-08-29 11:56:59 -0700

    added price

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
     attribute :name, :string
     attribute :brand, :string
     attribute :weight, :float
-
+    attribute :price, :integer
     has_many :ewastes
     has_many :requests, through: :ewastes
 end
